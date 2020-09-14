3cd782d8401623a817be97679774a7ddc3410411
Evan <3043537800@qq.com>
2020-09-14 15:01:07 +0800

    cmd-env

--- src/http/http.js
+++ src/http/http.js
@@ -4,9 +4,10 @@ import { ShowLoading, HideLoding } from '../components/loading'
 
 class Http {
     constructor() {
+        console.log(process.env)
         this.$http = axios.create()
         this.$http.defaults.timeout = 10000
-        this.$http.defaults.baseURL = ''
+        this.$http.defaults.baseURL = process.env.KG_API_URL ||''
         this.$http.defaults.headers = {
             "Content-Type": 'application/json;charset=UTF-8',
             //"Content-Type": 'application/x-www-form-urlencoded;charset=UTF-8',
